b235f576ccabf249085321904ae523ca24e703b4
Yasas Wijetilake <yasas.wijetilake@gmail.com>
2020-07-02 21:48:53 +0530

    vision mission

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,6 +140,15 @@
     time: 1000
   });
 
+    // Testimonials carousel (uses the Owl Carousel library)
+/*    $(".testimonials-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
+  */
+
   // Porfolio isotope and filter
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
